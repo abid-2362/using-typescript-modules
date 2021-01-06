06dd159a57e4747f6c762500d80fdef1a79b0a6b
Abid Ali <abdsoftfsd@gmail.com>
2021-01-06 19:32:35 +0500

    adds comment from the lecture

--- b.ts
+++ b.ts
@@ -11,6 +11,13 @@ console.log(reverse('learning'), isOdd(3));
 //   import('./a').then(a => console.log('The message is: ', a.aString));
 // }
 
-import * as config from './config.json';
 
-console.log(config.userSetting);
+/**
+ * This is not allowed, as we can see the error in the console if we load the app in browser.
+ * but as per the instructor, if required, some 3rd party library can solve this problem, Here is the error from console.
+ *
+ * Failed to load module script: The server responded with a non-JavaScript MIME type of "application/json". Strict MIME type checking is enforced for module scripts per HTML spec.
+ * */
+// import * as config from './config.json';
+// console.log(config.userSetting);
+console.log('will require some 3rd party library to import .json files as modules');
