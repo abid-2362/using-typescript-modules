584be2669e88352780bf6dafcdf43932aa337f73
Abid Ali <abdsoftfsd@gmail.com>
2021-01-06 19:27:40 +0500

    adds resolveJsonModule setting in tsconfig settings

--- b.ts
+++ b.ts
@@ -4,3 +4,13 @@ import * as strings from './a';
 console.log(strings.aString);
 
 import { reverse, isOdd } from './utils';
+
+console.log(reverse('learning'), isOdd(3));
+
+// if ( Math.random() >= 0.5 ) {
+//   import('./a').then(a => console.log('The message is: ', a.aString));
+// }
+
+import * as config from './config.json';
+
+console.log(config.userSetting);
